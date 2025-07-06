0ddddd550d23e1ffe2752f4c0ccf39d638fef6f7
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-07-06 17:37:07 +0400

    feat: add privacy

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,64 +1,16 @@
-import { useState } from "react";
-import SiteHead from "./components/SiteHead";
-import LanguageSwitcher from "./components/LanguageSwitcher";
-import GenerateButton from "./components/GenerateButton";
-import RelativeResult from "./components/RelativeResult";
-import getRandomNumberInRange from "./helpers/getRandomNumberInRange";
-import { generate, relativeFinish, wayToRelative } from "./constants/texts";
-import { ELang } from "./models/lang.enum";
-import "./App.css";
-
-const langs: ELang[] = [ELang.en, ELang.ru, ELang.am];
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  const [relative, setRelative] = useState("");
-  const [lang, setLang] = useState<ELang>(ELang.en);
-
-  const generateRelative = () => {
-    setRelative("");
-
-    const wayCount = getRandomNumberInRange(3, 8);
-    let closeRelative = "";
-    let prevRelativeWayIdx = -1;
-
-    for (let i = 0; i < wayCount; i++) {
-      const maxNum = wayToRelative[lang].length;
-      const relativeWayIdx = getRandomNumberInRange(0, maxNum);
-
-      if (relativeWayIdx === prevRelativeWayIdx) {
-        i--;
-        continue;
-      }
-
-      prevRelativeWayIdx = relativeWayIdx;
-      const relativeWay = wayToRelative[lang][relativeWayIdx];
-      closeRelative += `${relativeWay} `;
-    }
-
-    const relativeIdx = getRandomNumberInRange(0, relativeFinish[lang].length);
-    closeRelative += relativeFinish[lang][relativeIdx];
-
-    setRelative(closeRelative);
-  };
-
   return (
-    <>
-      <SiteHead lang={lang} />
-      <div className="container">
-        <div className="relative">
-          <LanguageSwitcher
-            langs={langs}
-            selectedLang={lang}
-            onChangeLang={(newLang) => {
-              setLang(newLang);
-              setRelative("");
-            }}
-          />
-          <GenerateButton onClick={generateRelative} label={generate[lang]} />
-          <RelativeResult text={relative} />
-        </div>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ website/src/Home.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import SiteHead from "./components/SiteHead";
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import GenerateButton from "./components/GenerateButton";
+import RelativeResult from "./components/RelativeResult";
+import getRandomNumberInRange from "./helpers/getRandomNumberInRange";
+import { generate, relativeFinish, wayToRelative } from "./constants/texts";
+import { ELang } from "./models/lang.enum";
+import "./Home.css";
+
+const langs: ELang[] = [ELang.en, ELang.ru, ELang.am];
+
+function Home() {
+  const [relative, setRelative] = useState("");
+  const [lang, setLang] = useState<ELang>(ELang.en);
+
+  const generateRelative = () => {
+    setRelative("");
+
+    const wayCount = getRandomNumberInRange(3, 8);
+    let closeRelative = "";
+    let prevRelativeWayIdx = -1;
+
+    for (let i = 0; i < wayCount; i++) {
+      const maxNum = wayToRelative[lang].length;
+      const relativeWayIdx = getRandomNumberInRange(0, maxNum);
+
+      if (relativeWayIdx === prevRelativeWayIdx) {
+        i--;
+        continue;
+      }
+
+      prevRelativeWayIdx = relativeWayIdx;
+      const relativeWay = wayToRelative[lang][relativeWayIdx];
+      closeRelative += `${relativeWay} `;
+    }
+
+    const relativeIdx = getRandomNumberInRange(0, relativeFinish[lang].length);
+    closeRelative += relativeFinish[lang][relativeIdx];
+
+    setRelative(closeRelative);
+  };
+
+  return (
+    <>
+      <SiteHead lang={lang} />
+      <div className="container">
+        <div className="relative">
+          <LanguageSwitcher
+            langs={langs}
+            selectedLang={lang}
+            onChangeLang={(newLang) => {
+              setLang(newLang);
+              setRelative("");
+            }}
+          />
+          <GenerateButton onClick={generateRelative} label={generate[lang]} />
+          <RelativeResult text={relative} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
